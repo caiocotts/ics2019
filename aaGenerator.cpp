1759aeb9aca7247bd0598d989ab1e0eb0fb44197
caio <caio@cotts.com.br>
2020-01-26 17:50:00 -0800

    improved aaGenerator

--- aaGenerator.cpp
+++ aaGenerator.cpp
@@ -1,35 +1,56 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
 using namespace std;
 
-void hline() {
+void hLine() {
   int length;
-  cout << "Enter lenght of line ";
+  cout << "Enter lenght of line(1-20): ";
   cin >> length;
+  while (length < 1 || length > 20) {
+    cout << "Invalid dimension!  The dimension must be between 1 and 20";
+    cout << "Enter lenght of line(1-20): ";
+    cin >> length;
+  }
+
   for (int i = 0; i < length; i++) {
     cout << "*";
   }
   cout << "\n";
 }
 
-void vline() {
+void vLine() {
   int height;
-  cout << "Enter height of line: ";
+  cout << "Enter height of line(1-20): ";
   cin >> height;
+  while (height < 1 || height > 20) {
+    cout << "Invalid dimension!  The dimension must be between 1 and 20";
+    cout << "Enter height of line(1-20): ";
+    cin >> height;
+  }
+
   for (int i = 0; i < height; i++) {
     cout << "*\n";
   }
 }
 
 void rect() {
-  int lenght;
+  int length;
   int height;
-  cout << "Enter lenght of rectangle: ";
-  cin >> lenght;
+  cout << "Enter length of rectangle: ";
+  cin >> length;
   cout << "Enter height of rectangle ";
   cin >> height;
+  while ((length < 1 || length > 20) || (height < 1 || height > 20)) {
+    cout << "Invalid dimension!  The dimension must be between 1 and 20\n";
+    cout << "Enter length of rectangle: ";
+    cin >> length;
+    cout << "Enter height of rectangle ";
+    cin >> height;
+  }
+
   for (int i = 0; i < height; i++) {
-    for (int i = 0; i < lenght; i++) {
+    for (int i = 0; i < length; i++) {
       cout << "*";
     }
     cout << "\n";
@@ -47,7 +68,7 @@ void lSlantTriangle() {
     cout << "\n";
   }
 }
-// ksdjblndvlsk
+
 void rSlantTriangle() {
   int height;
 
@@ -79,25 +100,43 @@ void isoTriangle() {
   }
 }
 
-// void doubleIsoTriangle() {
-//   int height;
-//   cout << "Enter height of triangle: ";
-//   cin >> height;
-//   for (int j = 1; j < height + 1; j++) {
-//     for (int s = 0; s > (height - j); s++) {
-//       cout << " ";
-//     }
-//     for (int i = 1; i > j * 2; i--) {
-//       cout << "*";
-//     }
-//     cout << "\n";
-//   }
-// }
+void doubleIsoTriangle() {
+  int height;
+  cout << "Enter height of triangle: ";
+  cin >> height;
+  height /= 2;
+  height += 1;
+  for (int j = height; j > 0; j--) {
+    for (int s = (height - j); s > 0; s--) {
+      cout << " ";
+    }
+    for (int i = 1; i < j * 2; i++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  for (int j = 2; j < height + 1; j++) {
+    for (int s = (height - j); s > 0; s--) {
+      cout << " ";
+    }
+    for (int i = 1; i < j * 2; i++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+}
 
 int main() {
   bool running = true;
+  int counter_hLine = 0;
+  int counter_vLine = 0;
+  int counter_rect = 0;
+  int counter_lSlantTriangle = 0;
+  int counter_rSlantTriangle = 0;
+  int counter_isoTriangle = 0;
+  int counter_doubleIsoTriangle = 0;
 
-  cout << "Welcome to the Ascii shape generator!";
+  cout << "Welcome to the Ascii shape generator!\n\n";
   int shapeIndex = 0;
   while (running) {
     cout << "This program draws the following shapes:\n   1) Horizontal "
@@ -114,32 +153,39 @@ int main() {
     }
     switch (shapeIndex) {
     case 1:
-      hline();
+      hLine();
+      counter_hLine += 1;
       break;
 
     case 2:
-      vline();
+      vLine();
+      counter_vLine += 1;
       break;
 
     case 3:
       rect();
+      counter_rect += 1;
       break;
 
     case 4:
       lSlantTriangle();
+      counter_lSlantTriangle += 1;
       break;
 
     case 5:
       rSlantTriangle();
+      counter_rSlantTriangle += 1;
       break;
 
     case 6:
       isoTriangle();
+      counter_isoTriangle += 1;
       break;
 
-      // case 7:
-      //   doubleIsoTriangle();
-      //   break;
+    case 7:
+      doubleIsoTriangle();
+      counter_doubleIsoTriangle += 1;
+      break;
 
     default:
       break;
@@ -147,7 +193,22 @@ int main() {
     char yesOrNo;
     cout << "Would you like to generate another shape (y/n)? ";
     cin >> yesOrNo;
+    while (tolower(yesOrNo) != 'n' && tolower(yesOrNo) != 'y') {
+      cout << "Invalid input!  Your input must be ‘y’ or ‘n’.\n";
+      cout << "Would you like to generate another shape (y/n)? ";
+      cin >> yesOrNo;
+    }
+
     if (tolower(yesOrNo) == 'n')
       running = false;
   }
+  cout << "Here is the summary of the shapes that were "
+          "generated:\n\n";
+  printf("%-30s %d\n", "Horizontal line: ", counter_hLine);
+  printf("%-30s %d\n", "Vertical line: ", counter_vLine);
+  printf("%-30s %d\n", "Rectangle: ", counter_rect);
+  printf("%-30s %d\n", "Left slant triangle: ", counter_lSlantTriangle);
+  printf("%-30s %d\n", "Right slant triangle: ", counter_rSlantTriangle);
+  printf("%-30s %d\n", "Isosceles triangle: ", counter_isoTriangle);
+  printf("%-30s %d\n", "Double cone: ", counter_doubleIsoTriangle);
 }
